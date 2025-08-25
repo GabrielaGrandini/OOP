8674df3ae3840e7c59c430a55f0d758258a0db87
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-08-25 15:25:41 -0300

    versão 2

--- EnumMeses/ObjetoMesesDoAno.java
+++ EnumMeses/ObjetoMesesDoAno.java
@@ -1,9 +1,11 @@
 package EnumMeses;
 
+
 public class ObjetoMesesDoAno {
     public static void main (String[] args){
-        for (MesesDoAno mes: MesesDoAno.values()){
-            System.out.println(mes);
-        }
+        MesesDoAno mes = MesesDoAno.Outubro;
+        System.out.println(mes);
+
     }
 }
+
