a9ad1c0a5111646cfe16e48480212d2fd5225334
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-08-25 15:24:16 -0300

    Add files via upload

---
+++ Modificador/ObjetoPessoaAutomovel.java
@@ -0,0 +1,29 @@
+package Modificador;
+import java.util.Scanner;
+
+public class ObjetoPessoaAutomovel {
+    public static void main (String [] args){
+        Scanner ler = new Scanner (System.in);
+        Pessoa pessoa = new Pessoa ();
+
+        String cor, nome;
+        int velocidade;
+
+        System.out.println("Informe a cor do carro: ");
+        cor = ler.next ();
+
+        System.out.println("Informeo nome do carro: ");
+        nome = ler.next ();
+
+        System.out.println("Informe a velocidade do carro: ");
+        velocidade = ler.nextInt ();
+
+        pessoa.compraCarro(nome,velocidade,cor);
+
+        Automovel automovel = pessoa.getAutomovel();
+
+        System.out.println("O carro comprado foi " + automovel.getNome()+ " da cor " + automovel.getCor() + " que atinge a velocidade de "+ automovel.getVelocidade());
+
+        ler.close();
+    }
+}
---
+++ Modificador/Pessoa.java
@@ -0,0 +1,48 @@
+package Modificador;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String profissao;
+    private Automovel automovel;
+
+
+    public void compraCarro (String nome, int velocidade, String cor) {
+        automovel = new Automovel();
+        automovel.setNome(nome);
+        automovel.setVelocidade (velocidade);
+        automovel.setCor (cor);
+    }
+
+    public Automovel getAutomovel() {
+        return automovel;
+    }
+
+    public void setAutomovel(Automovel automovel) {
+        this.automovel = automovel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+}
