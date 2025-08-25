ed9020d7f16f08dba863bad758af50eb6fe29e46
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-08-25 15:29:25 -0300

    Versão 1
    Apresenta todos os meses do ano

--- EnumMeses/ObjetoMesesDoAno.java
+++ EnumMeses/ObjetoMesesDoAno.java
@@ -1,11 +1,9 @@
 package EnumMeses;
 
-
 public class ObjetoMesesDoAno {
     public static void main (String[] args){
-        MesesDoAno mes = MesesDoAno.Outubro;
-        System.out.println(mes);
-
+        for (MesesDoAno mes: MesesDoAno.values()){
+            System.out.println(mes);
+        }
     }
 }
-
